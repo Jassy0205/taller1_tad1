b53a56b3e50afa948ed6f521429e69691723dd8f
Jassy0205 <a.z23042016a@gmail.com>
2022-09-07 15:40:45 -0500

    Primer punto

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+  Author: Jassy
+  Date: 07/09/2022
+  Description: python
+'''
+
+#1. Desde el archivo importamos la clase 
+from ast import Try
+from taller import solution
+
+inst_taller = solution()
+
+inst_taller.show_info()
---
+++ taller.py
@@ -0,0 +1,17 @@
+class solution: 
+
+    #1. Crear método inicializador de la clase
+    def __init__(self):
+        #2. Declaración de variables
+        self.productos_lacteos = []
+        self.productos_aseo = []
+        self.productos_granos = []
+
+    #3. Método que imprime los valores de las variables
+    def show_info(self):
+        print('Estamos trabajndo en Visual studio code')
+
+    
+
+
+    
